25d35d5332c839d082705e32956c08c155bf505a
Omekawy91 <156941679+Omekawy91@users.noreply.github.com>
2025-03-18 08:53:17 +0200

    Update weynak.js

--- weynak.js
+++ weynak.js
@@ -16,12 +16,13 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.error("Database connection error:", err));
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch(err => {
+    console.error("Database connection error:", err);
+});
 
 
 const transporter = nodemailer.createTransport({
