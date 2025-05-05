4477ce85423ce66b6f6224d635c6fac99cdc90f4
Omekawy91 <156941679+Omekawy91@users.noreply.github.com>
2025-05-05 16:17:57 +0300

    Update model.js

--- model.js
+++ model.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
-  phoneNumber: { type: String, required: true },
   password: { type: String, required: true },
   otp: { type: String, default: null },
   otp_expires_at: { type: Date, default: null }
