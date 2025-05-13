a35945bedadca32212797c36a5fcb99558ebe5dd
Omekawy91 <156941679+Omekawy91@users.noreply.github.com>
2025-05-13 21:18:48 +0300

    Update weynak.js

--- weynak.js
+++ weynak.js
@@ -36,7 +36,7 @@ const generateAccessToken = (user) => {
   return jwt.sign(
     { id: user._id, name: user.name, email: user.email },
     process.env.JWT_SECRET,
-    { expiresIn: "15m" }
+    { expiresIn: "50m" }
   );
 };
 
