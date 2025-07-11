bea4ef96b5150fe3898017be401c6815f020aedb
Omekawy91 <156941679+Omekawy91@users.noreply.github.com>
2025-07-11 23:24:08 +0300

    Update weynak.js

--- weynak.js
+++ weynak.js
@@ -10,7 +10,7 @@ const { User, Meeting, Participant, Movement, Notification, GroupMovement } = re
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
